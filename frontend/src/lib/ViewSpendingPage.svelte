00397a16dbbac1b6872bc01b9ae61d4bb673bab2
aidilrx04 <mrcrayv14@gmail.com>
2026-04-23 16:28:58 +0800

    change delete button color

--- frontend/src/lib/ViewSpendingPage.svelte
+++ frontend/src/lib/ViewSpendingPage.svelte
@@ -60,7 +60,9 @@
 	<div class="p-4 flex justify-between items-center">
 		<a href="/" class="text-sky-400">Back</a>
 		<h1 class="text-gray-100 font-semibold text-xl">Spending</h1>
-		<button class="text-rose-600" onclick={showModal}>delete</button>
+		<button class="text-rose-400 cursor-pointer" onclick={showModal}
+			>Delete</button
+		>
 	</div>
 	<section class="p-4 py-8 flex-1 flex flex-col">
 		<div class="flex-1">
