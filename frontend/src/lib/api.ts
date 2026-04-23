147600738614d320a69b744035126f406505e188
aidilrx04 <mrcrayv14@gmail.com>
2026-04-23 15:10:20 +0800

    add base to api

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,13 +1,13 @@
 const API_URL = import.meta.env.VITE_API_URL ?? 'http://127.0.0.1';
 
 export async function getSpendings() {
-	const res = await fetch(API_URL);
+	const res = await fetch(`${API_URL}/`);
 
 	return await res.json();
 }
 
 export async function createSpending(data: any) {
-	const res = await fetch(API_URL, {
+	const res = await fetch(`${API_URL}/`, {
 		method: "POST",
 		body: JSON.stringify({
 			'_method': 'POST',
