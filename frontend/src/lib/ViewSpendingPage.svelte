9627d189320ff4568ccf85d9eca564f166e604f1
aidilrx04 <mrcrayv14@gmail.com>
2026-04-23 16:14:37 +0800

    add loading state

--- frontend/src/lib/ViewSpendingPage.svelte
+++ frontend/src/lib/ViewSpendingPage.svelte
@@ -59,39 +59,68 @@
 	</div>
 	<section class="p-4 py-8 flex-1 flex flex-col">
 		<div class="flex-1">
-			<div class="mb-8">
-				<span
-					class="uppercase font-semibold tracking-wide block mb-2 text-gray-400"
-					>Amount</span
+			{#if loading}
+				<div
+					class="loading-spendings py-16 px-4 text-center flex items-center justify-center"
 				>
-				<span
-					class=" font-bold mb-4 text-4xl tracking-wide block text-violet-400"
-					>{currencyFormatter.format(spending.amount)}</span
-				>
-			</div>
+					<span class="animate-bounce">
+						<svg
+							class="animate-spin -ml-1 mr-3 size-8 text-violet-400"
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+						>
+							<circle
+								class="opacity-25"
+								cx="12"
+								cy="12"
+								r="10"
+								stroke="currentColor"
+								stroke-width="4"
+							></circle>
+							<path
+								class="opacity-75"
+								fill="currentColor"
+								d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+							></path>
+						</svg>
+					</span>
+				</div>
+			{:else}
+				<div class="mb-8">
+					<span
+						class="uppercase font-semibold tracking-wide block mb-2 text-gray-400"
+						>Amount</span
+					>
+					<span
+						class=" font-bold mb-4 text-4xl tracking-wide block text-violet-400"
+						>{currencyFormatter.format(spending.amount)}</span
+					>
+				</div>
 
-			<div class="mb-8">
-				<span
-					class="uppercase font-semibold tracking-wide block mb-2 text-gray-400"
-					>Date</span
-				>
+				<div class="mb-8">
+					<span
+						class="uppercase font-semibold tracking-wide block mb-2 text-gray-400"
+						>Date</span
+					>
 
-				<span class="text-gray-200 mb-4 text-lg block"
-					>{spending.date
-						? dateFormatter.format(spending.date)
-						: "Unspecified"}</span
-				>
-			</div>
+					<span class="text-gray-200 mb-4 text-lg block"
+						>{spending.date
+							? dateFormatter.format(spending.date)
+							: "Unspecified"}</span
+					>
+				</div>
 
-			<div class="mb-8">
-				<span
-					class="uppercase font-semibold tracking-wide block mb-4 text-gray-400"
-					>Note</span
-				>
-				<p class="text-gray-200 mb-4 text-lg block">
-					{spending.note}
-				</p>
-			</div>
+				<div class="mb-8">
+					<span
+						class="uppercase font-semibold tracking-wide block mb-4 text-gray-400"
+						>Note</span
+					>
+					<p class="text-gray-200 mb-4 text-lg block">
+						{spending.note}
+					</p>
+				</div>
+			{/if}
 		</div>
 	</section>
 	<div class="p-4">
