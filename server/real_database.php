8778e32610c97d84cb225c5d3980350e15df4d93
aidilrx04 <mrcrayv14@gmail.com>
2026-04-24 01:14:30 +0800

    refactor parsing env file

--- server/real_database.php
+++ server/real_database.php
@@ -1,5 +1,23 @@
 <?php
 
+function parseEnv(string $file)
+{
+	$env = file_get_contents($file);
+	$env = explode(PHP_EOL, $env);
+	$env = array_map(function ($value) {
+		return explode('=', $value, 2);
+	}, $env);
+
+	$env = array_reduce($env, function ($carry, $value) {
+		return [
+			...$carry,
+			$value[0] => $value[1]
+		];
+	}, []);
+
+	return $env;
+}
+
 $host = '127.0.0.1';
 $user = 'root';
 $pass = '';
@@ -8,25 +26,14 @@ $name = 'papan';
 $file = __DIR__ . '/.env';
 
 if (file_exists($file)) {
-	$envs = file_get_contents($file);
-	$envs = explode(PHP_EOL, $envs);
-	$envs = array_map(function ($value) {
-		return explode('=', $value, 2);
-	}, $envs);
+	$env = parseEnv($file);
 
-	$envs = array_reduce($envs, function ($carry, $value) {
-		return [
-			...$carry,
-			$value[0] => $value[1]
-		];
-	}, []);
-
-	$host = $envs['HOST'] ?? $host;
-	$user = $envs['USER'] ?? $user;
-	$pass = $envs['PASS'] ?? $pass;
-	$name = $envs['NAME'] ?? $name;
+	$host = $env['HOST'] ?? $host;
+	$user = $env['USER'] ?? $user;
+	$pass = $env['PASS'] ?? $pass;
+	$name = $env['NAME'] ?? $name;
 }
 
 $conn = mysqli_connect($host, $user, $pass, $name);
 
-$envs = null;
+$env = null;
