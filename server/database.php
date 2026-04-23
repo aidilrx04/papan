247f1a21bdeec7719b89c4d4030c529ee883a75f
aidilrx04 <mrcrayv14@gmail.com>
2026-04-23 16:56:04 +0800

    add .env handling

--- server/database.php
+++ server/database.php
@@ -5,4 +5,26 @@ $user = 'root';
 $pass = '';
 $name = 'papan';
 
+if (file_exists('.env')) {
+	$envs = file_get_contents('.env');
+	$envs = explode(PHP_EOL, $envs);
+	$envs = array_map(function ($value) {
+		return explode('=', $value, 2);
+	}, $envs);
+
+	$envs = array_reduce($envs, function ($carry, $value) {
+		return [
+			...$carry,
+			$value[0] => $value[1]
+		];
+	}, []);
+
+	$host = $envs['HOST'] ?? $host;
+	$user = $envs['USER'] ?? $user;
+	$pass = $envs['PASS'] ?? $pass;
+	$name = $envs['NAME'] ?? $name;
+}
+
 $conn = mysqli_connect($host, $user, $pass, $name);
+
+$envs = null;
