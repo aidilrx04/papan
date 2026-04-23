5a606c45a289d2b10c2e36eb76187789c84300e1
aidilrx04 <mrcrayv14@gmail.com>
2026-04-24 00:17:51 +0800

    refactor loading spinner out

--- frontend/src/lib/pages/ViewSpending.svelte
+++ frontend/src/lib/pages/ViewSpending.svelte
@@ -2,6 +2,7 @@
 	import { onMount } from "svelte";
 	import { deleteSpending, getSpending } from "../api";
 	import Modal from "../components/Modal.svelte";
+	import Spinner from "../components/Spinner.svelte";
 
 	const ID_RE = /\/spending\/(\d+)/;
 	const path = location.pathname;
@@ -74,26 +75,7 @@
 					class="loading-spendings py-16 px-4 text-center flex items-center justify-center"
 				>
 					<span class="animate-bounce">
-						<svg
-							class="animate-spin -ml-1 mr-3 size-8 text-violet-400"
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-						>
-							<circle
-								class="opacity-25"
-								cx="12"
-								cy="12"
-								r="10"
-								stroke="currentColor"
-								stroke-width="4"
-							></circle>
-							<path
-								class="opacity-75"
-								fill="currentColor"
-								d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-							></path>
-						</svg>
+						<Spinner />
 					</span>
 				</div>
 			{/if}
