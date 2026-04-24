94dc918912583f0e1ec599b58e11c55f01b39168
aidilrx04 <mrcrayv14@gmail.com>
2026-04-24 12:24:13 +0800

    copy server files to build dir

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,25 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import tailwindcss from '@tailwindcss/vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    svelte()
+    svelte(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: ['../server/**/*.{php,htaccess}'],
+          dest: '../',
+          rename: {
+            stripBase: 1
+          },
+
+        }
+      ],
+    })
   ],
   build: {
     outDir: 'dist/public'
