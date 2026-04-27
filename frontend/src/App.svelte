a9f99412d0133819a345d315fee2fee8a1f3397e
aidilrx04 <mrcrayv14@gmail.com>
2026-04-27 22:34:38 +0800

    match basic path param

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -5,11 +5,80 @@
 
 	let path = location.pathname;
 
-	const routes = {
+	const routes: {
+		[key: string]: any;
+	} = {
 		"/": HomePage,
 		"/spending/:id": ViewSpendingPage,
 		"*": NotFound,
 	};
+
+	function getActiveRoute() {
+		const path = location.pathname;
+		const routePaths = regexifyPaths(Object.keys(routes));
+
+		console.log(routePaths);
+
+		for (const { path: _p, regex } of routePaths) {
+			console.log(_p, regex, regex.test(path));
+			if (regex.test(path)) {
+				console.log("We should render", routes[_p]);
+			}
+		}
+	}
+
+	function regexifyPaths(paths: string[]) {
+		const regexified: {
+			path: string;
+			regex: RegExp;
+		}[] = [];
+
+		for (const path of paths) {
+			if (path === "*") {
+				regexified.push({
+					path,
+					regex: /^.*$/,
+				});
+				continue;
+			}
+
+			if (path.indexOf(":") < 0) {
+				regexified.push({
+					path,
+					regex: new RegExp(`^${path}$`),
+				});
+				continue;
+			}
+
+			// const param_re = /(:[a-zA-Z0-9_-]*)/g;
+			const param_re = /(:[^\/]+)/g;
+
+			const matches = path.match(param_re);
+			if (!matches)
+				throw new Error("Invalid condition. path should pass matches");
+
+			let transformedString = path;
+			for (const match of matches) {
+				transformedString =
+					transformedString.slice(
+						0,
+						transformedString.indexOf(match),
+					) +
+					"([^/]+)" +
+					transformedString.slice(
+						transformedString.indexOf(match) + match.length,
+					);
+			}
+			regexified.push({
+				path,
+				regex: new RegExp(`^${transformedString}$`),
+			});
+		}
+
+		return regexified;
+	}
+
+	getActiveRoute();
 </script>
 
 {#if path === "/"}
