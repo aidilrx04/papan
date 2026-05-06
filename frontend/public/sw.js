a386c1f35c0480877b4d1890c23d8fc2d340b2fd
aidilrx04 <mrcrayv14@gmail.com>
2026-05-07 05:07:19 +0800

    add pwa

---
+++ frontend/public/sw.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = "v0.3.0";
+const urlsToCache = ['index.html'];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,6 +1,15 @@
 <script lang="ts">
 	import { Router } from "sv-router";
 	import "./lib/router";
+
+	if ("serviceWorker" in navigator) {
+		window.addEventListener("load", () => {
+			navigator.serviceWorker
+				.register("/sw.js")
+				.then((reg) => console.log("Service Worker registered!"))
+				.catch((err) => console.log("Registration failed:", err));
+		});
+	}
 </script>
 
 <Router />
