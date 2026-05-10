1f45333d65de89d01b946ae6ab968b0c0de0b0ef
aidilrx04 <mrcrayv14@gmail.com>
2026-05-11 00:36:40 +0800

    refactor connection status

--- frontend/src/lib/pages/Home.svelte
+++ frontend/src/lib/pages/Home.svelte
@@ -16,6 +16,7 @@
 	} from "../db";
 	import * as api from "../api";
 	import { isInit, isUp } from "../healthcheck.svelte";
+	import ConnectionStatus from "../components/ConnectionStatus.svelte";
 
 	let loading = $state(true);
 	let spendingItems = $state<SpendingItemDetail[]>([]);
@@ -31,7 +32,7 @@
 		}, {}),
 	);
 
-	$inspect(spendingItems);
+	$inspect(spendingItems).with(console.debug);
 
 	let totalSpent = $derived.by(() => calculateTotalSpendings(spendingItems));
 
@@ -260,38 +261,7 @@
 				<div
 					class=" flex items-center justify-end px-4 py-2 gap-2 text-sm"
 				>
-					{#if !isUp() && !isInit()}
-						<span class="text-rose-600">Can't connect</span>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke-width="1.5"
-							stroke="currentColor"
-							class="size-5 text-rose-600"
-						>
-							<path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								d="m3 3 8.735 8.735m0 0a.374.374 0 1 1 .53.53m-.53-.53.53.53m0 0L21 21M14.652 9.348a3.75 3.75 0 0 1 0 5.304m2.121-7.425a6.75 6.75 0 0 1 0 9.546m2.121-11.667c3.808 3.807 3.808 9.98 0 13.788m-9.546-4.242a3.733 3.733 0 0 1-1.06-2.122m-1.061 4.243a6.75 6.75 0 0 1-1.625-6.929m-.496 9.05c-3.068-3.067-3.664-7.67-1.79-11.334M12 12h.008v.008H12V12Z"
-							/>
-						</svg>
-					{:else}
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke-width="1.5"
-							stroke="currentColor"
-							class="size-5 text-emerald-400"
-						>
-							<path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								d="M8.288 15.038a5.25 5.25 0 0 1 7.424 0M5.106 11.856c3.807-3.808 9.98-3.808 13.788 0M1.924 8.674c5.565-5.565 14.587-5.565 20.152 0M12.53 18.22l-.53.53-.53-.53a.75.75 0 0 1 1.06 0Z"
-							/>
-						</svg>
-					{/if}
+					<ConnectionStatus />
 				</div>
 			</div>
 		</div>
